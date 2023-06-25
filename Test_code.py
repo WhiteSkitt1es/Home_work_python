7fa6318546d5d1db3f806199137bb19846ee62af
Vado <vado@MBP-Paul.Dlink>
2023-06-25 18:21:48 +0300

    test code

---
+++ Test_code.py
@@ -0,0 +1,13 @@
+# # Task_stone_1
+# import string
+# string.ascii_lowercase
+
+# array = []
+
+# for i in range(0, 10):
+#     if i % 2 != 0:
+#         array.append(i + 1)
+#     else:
+#             array.append(string.ascii_lowercase[i])
+
+# print(*array)
