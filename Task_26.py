352554faf9c2979d365df2ccf0cbd6702a221a93
Vado <vado@MacBook-Pro-Paul.local>
2023-06-29 14:33:20 +0300

    Task_26 completed

--- Task_26.py
+++ Task_26.py
@@ -5,3 +5,15 @@
 
 # A = 3; B = 5 -> 243
 # A = 2; B = 3 -> 8 
+
+a = int(input('Введите число А: '))
+b = int(input('Введите число B: '))
+
+
+def recursionSum(a,b):
+    if b == 1:
+        return a
+    else:
+        return a * recursionSum(a,b - 1)
+    
+print(recursionSum(a,b))
